79c7f659416fa1f548c1274f7188fa02e9713f24
Iakovos Mastrogiannopoulos <iakmastro@outlook.com.gr>
2021-11-11 16:19:56 +0200

    Sonar: RestAPI established

---
+++ sonar_api/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import redis from 'redis'
+import cors from 'cors'
+
+// Express configuration
+const app = express()
+const port = 5000
+
+// Mongoose configuration
+const mongo_client = mongoose.createConnection('mongodb://dbuser:dbpass@mongo:27017')
+
+// Redis configuration
+// const redis_client = redis.createClient("redis://dbuser:dbpass@redis:6379/1")
+
+app.get("/ping", cors(), (req, res) => {
+	res.send("pong!")
+})
+
+app.listen(port, () => {
+	console.log(`Server has started at http://localhost:${port}`)
+})
