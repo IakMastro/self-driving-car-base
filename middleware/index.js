a4c63e2eabc9abd1716ded6eccb99269868bc443
Iakovos Mastrogiannopoulos <iakmastro@outlook.com.gr>
2021-12-21 20:01:15 +0200

    middleware init

---
+++ middleware/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import router from './routes.js'
+
+mongoose
+	.connect("mongodb://dbuser:dbpass@172.18.0.2:27017/test?authSource=admin", {
+		"useNewUrlParser": true
+	})
+	.then(() => {
+		const app = express()
+		app.use(express.json())
+		app.use("/api", router)
+
+		app.listen(8351, () => console.log("Middleware has started!"))
+	})
---
+++ middleware/models/Post.js
@@ -0,0 +1,8 @@
+import mongoose  from "mongoose"
+
+const schema = mongoose.Schema({
+  title: String,
+  content: String
+})
+
+export default mongoose.model("Post", schema)
