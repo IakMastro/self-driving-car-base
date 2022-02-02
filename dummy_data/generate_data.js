bfb9643223876016388d92b5cf0ac59d0394a09c
Your Name <iakmastro@outlook.com.gr>
2022-02-02 04:20:33 +0200

    Logic fault

--- dummy_data/generate_data.js
+++ dummy_data/generate_data.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
 
 axios.post('http://20.0.0.202:8351/api/sonar', {
-  sonar_front: Math.floor(Math.random() * (20 + 30 - 1) + 30),
-  sonar_back: Math.floor(Math.random() * (20 + 30 - 1) + 30)
+  sonar_front: Math.floor(Math.random() * (20 + 30 - 1) + 20),
+  sonar_back: Math.floor(Math.random() * (20 + 30 - 1) + 20)
 })
-axios.post('http://20.0.0.202:8351/api/speed', { speed: Math.floor(Math.random() * (5 + 10 - 1) + 10) })
-axios.post('http://20.0.0.202:8351/api/accelerate', { accelerate: Math.floor(Math.random() * (5 + 10 - 1) + 10) })
+axios.post('http://20.0.0.202:8351/api/speed', { speed: Math.floor(Math.random() * (5 + 10 - 1) + 5) })
+axios.post('http://20.0.0.202:8351/api/accelerate', { accelerate: Math.floor(Math.random() * (5 + 10 - 1) + 5) })
